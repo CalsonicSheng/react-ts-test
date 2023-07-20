245de2fd9ceac6fc3838d4d5c935207da911019f
CalsonicSheng <calson.developer@gmail.com>
2023-07-20 18:20:34 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { GlobalStateContext } from "./context/GlobalStateContext";
 import { useCounterHook } from "./hooks/useCounterHook";
 
 // outside functional component code will only run once ONLY UPON COMPONENT MOUNTING and does not subject to react re-rendering process to re-execute these codes
-// all declared variables (global) are accessible under the current functional component block under this file/module
 // by convention, we must have modifiers ("const" or "let") for each declared variable (if not, then it will be treated as global variable under "window" object)
 const title: string = "Parent Component";
 const c1Props: { a: number; b: number } = { a: 5555, b: 6666 };
@@ -18,17 +17,17 @@ function App(): ReactElement {
   console.log("parent component run");
 
   // again, all the "states" when declared are only locally accessible in the TARGET component where they declared and ARE NOT shareable/accessible by other child components.
-  // any state update function should only be called under either event handler or lifecycle hook callback function, CAN NOT call direcly under component function body to avoid infinite loop.
+  // any state update function should only be called (after declaration) under either event handler or lifecycle hook. CAN NOT call direcly under component function body to avoid infinite loop.
   // we can also see if the state update results the same previous state value, then the component will NOT re-render.
   const [parentGlobalState, setParentGlobalState] = useState<number>(11111);
   const { counter, setCounterHandler }: { counter: number; setCounterHandler: () => void } = useCounterHook("parent");
 
-  // batch state and state updates
-  // we can see react will batch all the state updates when they are grouped under the single event handler function
+  // batch state updates
+  // we can see react will batch all the state updates when they are grouped under the single event handler function call
   // state2 update uses state1 value, but this state1 value is not the latest state1 value, but the state1 value when the event handler function is called (very important)
   // this meanings the batch update uses the latest state value BEFORE the event handler function is called, but not the latest state value AFTER the state update function is called
   // if state2 update needs to use the latest state1 value, then we need to use "useEffect" hook to run the state2 update function AFTER the state1 update function is completed
-  // this is the problem of state update ordering, we can use "useEffect" hook to solve this problem (very important)
+  // this is the STATE UPDATE ORDERING, we can use "useEffect" hook to solve this problem (very important)
   const [state1, setState1] = useState<number>(333);
   const [state2, setState2] = useState<number>(666);
 
--- src/hooks/useCounterHook.tsx
+++ src/hooks/useCounterHook.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 
-// we have confirmed that THE SAME customized hooks function when called under DIFFERENT MULTIPLE COMPONENTS are only local to the TARGET component where it is called
 // all the states/logics written under the customized hooks ARE LOCAL TO THE COMPONENT WHERE THE HOOK IS CALLED
 // if "useEffect" is also called under the customized hooks, it STILL FOLLOWS THE RULE OF "RUN AFTER ALL ELEMENTS AND CHILD COMPONENTS ARE RENDERED" UNDER THE TARGET COMPONENT WHERE THE
 // CUSTOMIZED HOOK IS CALLED.
-// also, we have tested that only when the state IS UPDATED, THEN THE TARGET COMPONENT WILL RE-RENDER | NOT WHEN THE STATE UPDATE FUNCTION IS CALLED THAT GIVES THE SAME STATE VALUE
-// "re-rendering" only happens under the target component where the associated STATE IS DECLARED WHEN IT IS UPDATED TO DIFFERENT VALUE (EVEN IF THE STATE IS NOT USED AFTER DECLARATION)
-// COMPONENT RENDEREDING IS VERY COMPONENT-SPECIFIC, ONLY THE STATE THAT IS ORIGINALLY DECLARED TO A SPECIFIC COMPONENT, WILL TRIGGER COMPONENT RE-RENDER IF THEIR VALUE IS UPDATED
+// also we have tested that target component re-rendering only happens if the state declared under such target component is updated to different value (not when update function is called everytime).
+// "re-rendering" ONLY happens when the associated STATE IS DECLARED WHEN IT IS UPDATED TO DIFFERENT VALUE (EVEN IF THE STATE IS NOT USED AFTER DECLARATION).
+// react "re-rendering" will ONLY trigger the TARGET COMPONENT where that state variable IS ORIGINALLY DECLARED to re-render (First) + any child components that is under the target component.
 
-// additional state management notes:
+// Additional state management notes:
 // When a state or prop changes, React schedules a re-render, during which it constructs a new react virtual DOM, then it compares this tree with the previous one, and then it updates the
 // actual DOM to match the new tree (This process is optimized to minimize the actual DOM operations, which are expensive)
-// Batching state update: multiple state updates in React can occur in a batch through a event handling or lifecycle methods, causing a single re-render instead of multiple ones to optimize performance.
-// Dependent state updates:
+// Batching state update: multiple state updates in React can occur in a batch through a event handling or lifecycle methods call under a single re-render instead of multiple ones to optimize performance.
+// Batching state update does not perform for the dependent state update, which means the state update function will use the latest state value BEFORE UPDATE, but not the latest state value
+// after the state update function is called.
 
 export function useCounterHook(componentTitle: string): { counter: number; setCounterHandler: () => void } {
   console.log(`${componentTitle} useCounterHook run`);
